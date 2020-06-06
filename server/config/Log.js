13b7c79d839ad4c82eefbc75e4fb7045d39cb50d
Ajeet Singh <ajeets_1971@hotmail.com>
2020-06-06 11:39:47 +0530

    add log dir name.

--- server/config/Log.js
+++ server/config/Log.js
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import winston from 'winston';
-import config from '../config/environment'
+import config from '../config/environment';
+
 require('winston-daily-rotate-file');
 
 class Log {
@@ -21,26 +22,30 @@ class Log {
     }
 
     static padRight(str, padString, length) {
-        while (str.length < length)
+        while(str.length < length) {
             str = str + padString;
+        }
         return str;
     }
 
     /* Init stackify object and logger objects */
     static logInit() {
 
-        const logFormatter = function (options) {
+        let filePath = config.logFile.filePath;
+        console.log('Log path', filePath);
+
+        const logFormatter = function(options) {
             // Return string will be passed to logger.
             return '[' + Log.padRight(options.level.toUpperCase(), ' ', 5) + '][' + options.timestamp() + ']- ' +
                 (options.message ? options.message : '') +
-                (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '' );
+                (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '');
         };
 
-        const timestamp = function () {
-            return moment(new Date()).format('YYYY-MM-DD hh:mm:ss.SSSSSS');
+        const timestamp = function() {
+            return moment(new Date())
+                .format('YYYY-MM-DD hh:mm:ss.SSSSSS');
         };
 
-        let filePath = config.logFile.filePath;
         this.Log = new (winston.Logger)({
             transports: [
                 new (winston.transports.DailyRotateFile)({
@@ -65,10 +70,11 @@ class Log {
 
     /* Write log to file and/or stackify */
     static writeLog(Level, message, uniqueId = null) {
-        if (uniqueId)
+        if(uniqueId) {
             this.Log.log(Level, '[' + uniqueId + '] ' + message);
-        else
+        } else {
             this.Log.log(Level, message);
+        }
     }
 }
 
